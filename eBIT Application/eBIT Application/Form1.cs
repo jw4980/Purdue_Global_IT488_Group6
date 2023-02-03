eb08e9739c8f9de27e853ae12c5ea4d7d1539818
day8806 <74009379+day8806@users.noreply.github.com>
2023-02-02 21:32:47 -0500

    Add files via upload

---
+++ eBIT Application/eBIT Application/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eBIT_Application
+{
+    public partial class AddInventory : Form
+    {
+        public AddInventory()
+        {
+            InitializeComponent();
+        }
+
+        private async void AddInventory_Load(object sender, EventArgs e)
+        {
+            inventoryBindingSource.DataSource = new inventory();
+        }
+
+        private async void buttonAdd_Click(object sender, EventArgs e)
+        {
+            eBITEntities db = new eBITEntities();
+            db.inventories.Add(inventoryBindingSource.Current as inventory);
+            int result = await db.SaveChangesAsync();
+            if (result > 0)
+                MessageBox.Show("Data has been entered successfully");
+        }
+    }
+}
